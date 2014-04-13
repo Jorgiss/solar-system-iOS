feae5da8cef8e0c27034198b9906ac9f39deac9e
Andrius Steponavicius <andrius@ilgbusiness.co.uk>
2014-04-13 16:47:57 +0100

    Initial Commit

---
+++ solarsystemTests/solarsystemTests.m
@@ -0,0 +1,34 @@
+//
+//  solarsystemTests.m
+//  solarsystemTests
+//
+//  Created by Active-ILG on 13/04/2014.
+//  Copyright (c) 2014 Andrius. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface solarsystemTests : XCTestCase
+
+@end
+
+@implementation solarsystemTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
